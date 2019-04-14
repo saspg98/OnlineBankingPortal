54db528e3ba484ea43b19f4d8584578bb94f0c93
GedduWarkh <gedduWarkh@users.noreply.github.com>
2019-04-14 20:12:31 +0530

    Add AccountSummaryVM
    Also changed User, BankAccount and UserDataModel

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,5 +2,14 @@ package model;
 //POJO representing the user
 public class User {
     //TODO: Add attributes
+    String name;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
