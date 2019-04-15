85b69438abf8ce7e758e2302d23d1062bc64d7d7
saspg98 <sauravgupta97@gmail.com>
2019-04-15 12:31:11 +0530

    javaFx working perfectly with main code. Pls read the comment above overrided method(where FXMLoader instance is used.

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,12 +1,30 @@
 import connection.Connection;
 import connection.LocalConnection;
 import database.Database;
-import io.reactivex.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import static java.lang.System.out;
 
-public class MainApplication {
+public class MainApplication extends Application {
+
+    //Download javaFx scene builder 2.0 from oracle website and add path of exe to File/Settings/Language&Framework/JavaFX in intellij
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String ... args ){
+
+        launch(args);
+
         out.println("Welcome to the commandline interface of our client application!");
         out.println("Initializing Database instance");
         Connection con = new LocalConnection();
