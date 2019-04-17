f3e3051304f1f1ceb4dc6b5606f452b0cc8a38d2
Pranek Gupta <33083312+PRANEK@users.noreply.github.com>
2019-04-17 05:45:12 +0530

    Layouts uploaded

---
+++ src/main/java/ui/controllers/LoginScreenController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbmsproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Pranek
+ */
+public class LoginScreenController implements Initializable {
+
+   
+
+  
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+}
--- src/main/java/ui/controllers/ViewAddedPayeeController.java
+++ src/main/java/ui/controllers/ViewAddedPayeeController.java
@@ -1,32 +1,24 @@
-package ui.controllers;/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package dbmsproject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author Pranek
  */
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
+public class ViewAddedPayeeController implements Initializable {
 
-        System.err.println("Sign Up button clicked");
-        return;
-    }
-    
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
